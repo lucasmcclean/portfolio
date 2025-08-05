ec81c49c64a0ab40cec7674281b50b99234eab86
Lucas McClean <lucksdeveloper@gmail.com>
2025-08-05 20:32:45 +0200

    feat(articles): restyle, add nav links

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { ArticleMeta } from '$lib/articles';
 	import ArticleCollectionMeta from '$lib/components/ArticleCollectionMeta.svelte';
+	import Link from '$lib/components/Link.svelte';
 	import { SITE_NAME } from '$lib/consts.js';
 
 	const { data } = $props();
@@ -15,16 +16,23 @@
 />
 
 <div class="xs:text-lg mx-auto flex max-w-2xl flex-col items-center px-8">
-	<main class="w-full min-h-screen">
-		<header class="my-16">
-			<h1 class="xs:text-6xl text-4xl flex font-semibold tracking-tight sm:text-7xl">
-				All Articles
-				<span class="text-primary flex flex-grow justify-around px-2" aria-hidden="true">
-					<span>/</span><span>/</span><span>/</span><span>/</span><span>/</span>
-				</span>
-			</h1>
-		</header>
+	<header class="xs:self-start w-fit self-center pt-16 pb-0">
+		<h1 class="xs:text-6xl text-4xl font-semibold tracking-tight sm:text-7xl">Articles</h1>
+	</header>
 
+	<nav class="xs:justify-start flex w-full justify-center py-4 text-lg sm:justify-between">
+		<ul class="hidden gap-x-4 sm:flex">
+			<li><Link href="/">//LM</Link></li>
+		</ul>
+
+		<ul class="flex gap-x-4">
+			<li><Link href="/">Home</Link></li>
+			<li><Link href="/about">About</Link></li>
+			<li><Link href="/now">Now</Link></li>
+		</ul>
+	</nav>
+
+	<main class="w-full">
 		{#if articles.length === 0}
 			<p>No articles found.</p>
 		{:else}
