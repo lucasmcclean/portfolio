26c5d485f740c032b2ce75b70d6ff24589ec0ea7
Lucas McClean <lucksdeveloper@gmail.com>
2025-08-18 20:17:42 -0400

    feat(now): fill out the now section

--- src/routes/now/+page.svelte
+++ src/routes/now/+page.svelte
@@ -28,7 +28,86 @@
 		</ul>
 	</nav>
 
-	<main id="main-content" class="xs:space-y-16 my-8 w-full space-y-8"></main>
+	<main id="main-content" class="xs:space-y-16 my-8 w-full space-y-8">
+		<section class="space-y-4">
+			<h3 class="text-2xl font-semibold">Academics</h3>
+			<ul class="list-disc space-y-4 font-light">
+				<li>
+					<p>
+						Entering my second year as an Honors CS student at
+						<abbr title="University of Central Florida">UCF</abbr>.
+					</p>
+				</li>
+				<li>
+					<p>
+						Serving as the Technology Committee Coordinator for the Florida Delta chapter of the <Link
+							href="https://tbp.org"
+							external>Tau Beta Pi</Link
+						> engineering honor society.
+					</p>
+				</li>
+			</ul>
+		</section>
+
+		<section class="space-y-4">
+			<h3 class="text-2xl font-semibold">Work</h3>
+			<ul class="list-disc space-y-4 font-light">
+				<li>
+					<p>
+						I’m currently working as a
+						<Link
+							href="https://techrangers.cdl.ucf.edu/techrangers_current/lucas_mcclean.html"
+							external>Techranger</Link
+						> at UCF, where I help build and maintain tools using Python, Docker, JS, HTML, and CSS.
+					</p>
+				</li>
+			</ul>
+		</section>
+
+		<section class="space-y-4">
+			<h3 class="text-2xl font-semibold">Projects</h3>
+			<ul class="list-disc space-y-4 font-light">
+				<li>
+					<p>
+						<Link href="https://github.com/lucasmcclean/limitlink/tree/v2" external>LimitL.ink</Link
+						> is a URL shortener with usage limits and expiration. Built with <strong>Go</strong>,
+						<strong>Docker</strong>, and <strong>SvelteKit</strong>.
+					</p>
+				</li>
+				<li>
+					<p>
+						<Link href="https://crates.io/crates/kvd" external>KVD</Link> is a key-value differ that
+						is generic over file types. Built with <strong>Rust</strong>.
+					</p>
+				</li>
+			</ul>
+		</section>
+
+		<section class="space-y-4">
+			<h3 class="text-2xl font-semibold">Personal</h3>
+			<ul class="list-disc space-y-4 font-light">
+				<li>
+					<p>
+						I've been reading philosophical fiction. Most recently I finished Crime and Punishment
+						and I'm now reading Notes from Underground.
+					</p>
+				</li>
+				<li>
+					<p>
+						Switching from Arch Linux to Void Linux and polishing up my <Link
+							href="https://github.com/lucasmcclean/.dotfiles"
+							external>NeoVim config</Link
+						>.
+					</p>
+				</li>
+				<li>
+					I’ve started learning French by reading <em>Le Petit Prince</em>.
+				</li>
+			</ul>
+		</section>
+
+		<p class="xs:text-base text-end text-sm italic opacity-80">Last updated: August 2025</p>
+	</main>
 
 	<footer class="xs:text-base mt-auto w-full space-y-2 py-8 text-sm opacity-80">
 		<div class="flex flex-wrap justify-between">
