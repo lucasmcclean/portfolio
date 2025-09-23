9bd77cbf9a68612fabb38bf3dbb915c9683073ea
Lucas McClean <lucksdeveloper@gmail.com>
2025-09-23 09:31:46 -0400

    fix(sitemap): add image namespace

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -22,7 +22,10 @@ export const GET: RequestHandler = () => {
 	return new Response(
 		`
     <?xml version="1.0" encoding="UTF-8" ?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset 
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+    >
       <url>
         <loc>${BASE_URL}</loc>
         <image:image>
