fcafceebb386b26d919d3879f863155cc6478111
Lucas McClean <lucksdeveloper@gmail.com>
2025-10-07 19:41:50 -0400

    feat(site): increase standard width

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	let { children } = $props();
 </script>
 
-<div class="xs:text-lg mx-auto flex max-w-2xl flex-col items-center px-8">
+<div class="xs:text-lg mx-auto flex max-w-3xl flex-col items-center px-8">
 	<a href="#main-content" class="sr-only focus:not-sr-only">Skip to main content</a>
 	{@render children()}
 </div>
