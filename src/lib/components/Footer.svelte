e689c1b65d66f64e142b1ba719d02d594d5310ce
Lucas McClean <lucksdeveloper@gmail.com>
2025-11-10 19:51:30 -0500

    refactor(footer): change wording

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="xs:text-base mt-auto w-full space-y-2 pt-8 pb-16 text-sm opacity-80">
 	<div class="flex flex-wrap justify-between">
-		<p>&copy; 2025 Lucas McClean. All rights reserved.</p>
+		<p>Lucas McClean – servus Iesu Christi</p>
 		<p class="font-display text-primary ms-auto">//</p>
 	</div>
 </footer>
