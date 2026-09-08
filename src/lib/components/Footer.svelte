a2cd0224549d6eda4430a6946d417a43524a0634
Lucas McClean <lucksdeveloper@gmail.com>
2026-09-08 03:43:55 -0400

    feat(footer): add the rangerverse

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,13 @@
 <footer class="xs:text-base mt-auto w-full space-y-2 pt-8 pb-16 text-sm opacity-80">
 	<div class="flex flex-wrap justify-between">
 		<p class=" tracking-wide italic">Lucas McClean</p>
+		<nav class="mx-auto rangerverse w-fit flex gap-4">
+			<a href="https://edirbcmessej.github.io/the.rangerver.se/prev?from=lucas">← prev</a>
+			<a href="https://edirbcmessej.github.io/the.rangerver.se/random?from=lucas"
+				>✦ rangerverse</a
+			>
+			<a href="https://edirbcmessej.github.io/the.rangerver.se/next?from=lucas">next →</a>
+		</nav>
 		<p class="font-display text-primary ms-auto">//</p>
 	</div>
 </footer>
